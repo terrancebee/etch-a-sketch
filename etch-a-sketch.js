34866c6812ae628f49d8f12c5c37d319ad3eb761
Terrance Brown <terrancebee@gmail.com>
2020-08-06 11:46:44 -0500

    fixed the density quirks where if you cancel it wont create grid and some cleanup

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,59 +1,79 @@
 const resetButton = document.querySelector(".js-reset");
 const changeGridButton = document.querySelector(".js-change-grid");
 const grid = document.querySelector(".js-grid");
+const DEFAULT_GRID_SIZE = 16;
 
-const setGrid = (grid, number = 16) => {
-    setGridSize(grid, number);
+function setGrid(grid, size = DEFAULT_GRID_SIZE) {
+  setGridSize(grid, size);
+  createGrid(grid, size);
+}
 
-    for (let i = 0; i < number; i++) {
-        for (let i = 0; i < number; i++) {
-            let square = document.createElement("div");
-            square.classList.add("square");
-            grid.appendChild(square);
-        }
+function createGrid(grid, size) {
+  for (let i = 0; i < size; i++) {
+    for (let i = 0; i < size; i++) {
+      let square = document.createElement("div");
+      square.classList.add("square");
+      grid.appendChild(square);
     }
+  }
 }
 
-const setGridSize = (grid, number) => {
-    grid.style.gridTemplateColumns = `repeat(${number}, 1fr)`;
-    grid.style.gridTemplateRows = `repeat(${number}, 1fr)`;
+function setGridSize(grid, size) {
+  currentGridSize = size;
+  grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 }
 
-const deleteChildren = (node) => () => {
-    while(node.lastChild) {
-        node.removeChild(node.lastChild);
-    }
+function deleteChildren(node) {
+  while (node.lastChild) {
+    node.removeChild(node.lastChild);
+  }
 }
 
-const deleteGridChildren = deleteChildren(grid);
-
-const resetGrid = (grid, number = 16) => {
-    deleteGridChildren();
-    setGrid(grid, number);
+function resetGrid(grid, size = DEFAULT_GRID_SIZE) {
+  deleteChildren(grid);
+  setGrid(grid, size);
 }
 
-const clearGrid = (grid) => {
-    const squares = grid.querySelectorAll(".square");
-    [...squares].forEach((square) => { square.removeAttribute("style") });
+function clearGrid(grid) {
+  for (let square of grid.querySelectorAll(".square")) {
+    square.removeAttribute("style");
+  }
 }
 
-resetGrid(grid);
-
-resetButton.addEventListener("click", (evt) => { clearGrid(grid) });
+resetButton.addEventListener("click", (evt) => {
+  clearGrid(grid)
+});
 
 changeGridButton.addEventListener("click", (evt) => {
-    const number = prompt("Pick a number between 2 and 100 to change the square size.");
-    if (number < 2 || number > 100) {
-        const number = prompt("Please pick a number between 2 and 100 please.");
+  let userInput = prompt("Pick a number between 2 and 100 to change the square size.");
+  let number = parseInt(userInput, 10);
+
+  while (userInput != null) {
+    if (number > 2 && number < 100) break;
+
+    if (Number.isNaN(number)) {
+      userInput = prompt("Oops, that's not a number! We need one between 2 and 100!")
     } else {
-        resetGrid(grid, number);
+      userInput = prompt("Please pick a number between 2 and 100 please.");
     }
+    
+    number = parseInt(userInput, 10);
+  }
+
+  if (userInput == null) return;
+
+  resetGrid(grid, number);
 });
 
 grid.addEventListener("mouseover", (evt) => {
-    if (evt.target.classList.contains("square")) {
-        const opacity = getComputedStyle(evt.target).getPropertyValue("--opacity");        
-        if (opacity >= 1) return; 
-        evt.target.style.setProperty("--opacity", parseFloat(opacity, 10) + .1);
-    }
+  if (evt.target.classList.contains("square")) {
+    const opacity = getComputedStyle(evt.target).getPropertyValue("--opacity");
+
+    if (opacity >= 1) return;
+
+    evt.target.style.setProperty("--opacity", parseFloat(opacity, 10) + .1);
+  }
 });
+
+resetGrid(grid);
